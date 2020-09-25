6dae02470db1545683ca9fffe6375ea8b2283e62
Aleksandr Shpak <shpak.aleks@gmail.com>
2020-09-25 21:28:40 +0400

    Initial project structure

---
+++ src/main/java/com/ishikawa/musicbox/MusicboxApplication.java
@@ -0,0 +1,13 @@
+package com.ishikawa.musicbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishikawa/musicbox/MusicboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishikawa.musicbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
